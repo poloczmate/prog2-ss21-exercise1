5b282a09cfbb298c4b3c58fea2a4a789776f5ca6
poloczmate <48856366+poloczmate@users.noreply.github.com>
2021-03-08 22:26:18 +0100

    Length check ready

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,12 @@
 public class App {
-    public static void main(String[] args){
-        System.out.println("Hello World");
+    public static boolean checkPassword(String toCheck){
+        // TODO: 08.03.21 Klein und Gross auch
+        // TODO: 08.03.21 Muss Zahlen
+        // TODO: 08.03.21 min 1 Ziffer von "()#$?!%/@", andere nicht erlaubt
+        // TODO: 08.03.21 max 2 Zahlen im Reihenfolge
+        // TODO: 08.03.21 maximal 3 Zahlen nacheinander
+        if (toCheck.length() > 25 || toCheck.length() < 8) return false;
+
+        return true;
     }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,4 +2,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
+    @Test
+    public void test_length_1(){
+        String toCheck = "AbC12(asdasd";
+        assertTrue(App.checkPassword(toCheck));
+    }
+    @Test
+    public void test_length_2(){
+        String toCheck = "AbC12";
+        assertFalse(App.checkPassword(toCheck));
+    }
+    @Test
+    public void test_length_3(){
+        String toCheck = "AbC12(asdasdAbC12(asdasdAbC12(asdasdAbC12(asdasd";
+        assertFalse(App.checkPassword(toCheck));
+    }
 }
